7f400add670a7727dc4e22dbc8987753c844f16b
HaloGamer33 <106188903+HaloGamer33@users.noreply.github.com>
2025-11-25 16:40:31 -0600

    feat: temporal

--- index.js
+++ index.js
@@ -6,11 +6,19 @@ const userRoutes = require('./src/routes/userRoutes');
 
 app.use(express.json());
 
-app.use(cors({
-  origin: ["http://localhost:8080"],
-  credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
-}));
+// app.use(cors({
+//   origin: ["http://localhost:8080"],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// }));
+//
+// app.use(cors({
+//   origin: ["http://localhost:5173"],
+//   credentials: true,
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+// }));
+
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log("Request Origin:", req.headers.origin);
